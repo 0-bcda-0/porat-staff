38384d5ec15569163cd1e6493604a49edf3b84cf
bcda <j.jurjec@gmail.com>
2022-11-13 23:37:40 +0100

    JJ - Redesign & Popup creation
    Uveo glass morphism, napravio popup element, te potvrdu o rezervaciji.

--- __header-footer/footer.php
+++ __header-footer/footer.php
@@ -2,6 +2,7 @@
 
 echo '
     <script src="pwa.js"></script>
+    <script src="__js/reservations_popup.js"></script>
     </body>
 </html>
 ';
---
+++ __js/reservations_popup.js
@@ -0,0 +1,13 @@
+function popup() {
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
+
+function delete_popup() {
+    var popupWindow = document.getElementById('popupWindow');
+    popupWindow.classList.toggle('active');
+    var deleteWindow = document.getElementById('deleteWindow');
+    deleteWindow.classList.toggle('active');
+}
